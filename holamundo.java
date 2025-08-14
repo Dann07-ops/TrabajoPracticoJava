7bb60fc77970471b62d3c6fb83be9a256ed48b81
Daniel <danielalderete513@gmail.com>
2025-08-13 23:33:56 -0300

    Subiendo proyecto Java

---
+++ holamundo.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo.java;
+
+//import static java.lang.Boolean.TRUE;
+
+//import java.util.Scanner;
+
+
+/**
+ *
+ * @author User
+ */
+public class holamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Ejercicio 2
+        /* System.out.println("¡Hola, Java!");
+        // TODO code application logic here
+         */
+        // Ejercicio 3
+        /*String nombre = "Daniel";
+        int edad = 25;
+        double altura= 1.5;
+        boolean estudiante= TRUE;
+        
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(altura);
+        System.out.println(estudiante);*/
+        
+        //Ejercicio4
+        
+       /* Scanner input= new Scanner(System.in);
+        String nombre;
+        System.out.println("Ingrese su nombre: ");
+        nombre= input.nextLine();
+       
+        int edad;
+        System.out.println("Ingrese su edad: ");
+        edad= input.nextInt();
+        
+        System.out.println("Hola " + nombre + ",tienes " + edad + " años");*/
+       
+       //Ejercicio 5
+       
+        /*Scanner input= new Scanner(System.in);
+        
+        int num_1, num_2;
+        System.out.println("Ingrese un número entero: ");
+        num_1 = input.nextInt();
+        
+        System.out.println("Ingrese un segundo número entero: ");
+        num_2 = input.nextInt();
+        
+        System.out.println(num_1 + num_2);
+        System.out.println(num_1 - num_2);
+        System.out.println(num_1 * num_2);
+        System.out.println(num_1 / num_2);*/
+        
+        // Ejercicio 6
+        
+       /* System.out.println("Nombre: Juan Perez\nEdad: 30 años\nDireccion:\"Calle falsa 123\"");*/
+       
+       // Ejercicio 8
+       // Parte A
+       
+      /* Scanner input= new Scanner(System.in);
+       
+       int num1, num2;
+       
+        System.out.println("Ingrese un numero entero: ");
+        num1= input.nextInt();
+        System.out.println("Ingrese otro numero entero: ");
+        num2= input.nextInt();
+        
+        System.out.println(num1 / num2);*/
+        
+        // Parte B 
+        
+       // Scanner input= new Scanner(System.in);
+        
+       /* double numero_1, numero_2;
+        
+        System.out.println("Ingrese un numero: ");
+        numero_1= input.nextDouble();
+        System.out.println("Ingrese un segundo numero: ");
+        numero_2 = input.nextDouble();
+        
+        System.out.println(numero_1 / numero_2);*/
+       
+       
+       // Ejercicio 10
+       
+       int a = 5; 
+       int b = 2; 
+       int resultado = a / b; 
+       System.out.println("Resultado: " + resultado); 
+        
+        
+        
+        
+    
+    }
+    
+}
